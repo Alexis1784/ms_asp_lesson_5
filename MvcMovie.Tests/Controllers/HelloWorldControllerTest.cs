3d7be9481292e2275879318d0c3f664b02ef1027
Alex <alexis1784@gmail.com>
2016-01-06 05:55:04 +0500

    added test for fact creating of HelloWorldController instance

---
+++ MvcMovie.Tests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie.Controllers;
+
+namespace MvcMovie.Tests.Controllers
+{
+    [TestClass]
+    public class HelloWorldControllerTest
+    {
+        [TestMethod]
+        public void HelloWorldControllerIsNotNull()
+        { 
+            // Arrange, Act
+            HelloWorldController cont = new HelloWorldController();
+
+            // Assert
+            Assert.IsNotNull(cont);
+        }
+    }
+}
